08aca7bfaa2fdef43fa9e32b1902b693de1cea1a
Anatolij Dejneko <anatoliideineko@gmail.com>
2021-06-30 18:30:49 +0200

    переделал на базе класса

--- src/index.js
+++ src/index.js
@@ -1,30 +1,52 @@
 import './sass/main.scss';
 
-const CountdownTimer = function ({ selector, targetDate }) {
-  this.selector = selector;
-  this.targetDate = targetDate;
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = targetDate;
 
-  this.showTimer = function () {
-    const refTimer = document.querySelector(this.selector);
-    const daysRef = refTimer.querySelector('[data-value="days"]');
-    const hoursRef = refTimer.querySelector('[data-value="hours"]');
-    const minsRef = refTimer.querySelector('[data-value="mins"]');
-    const secsRef = refTimer.querySelector('[data-value="secs"]');
+    this.days = this.selector.querySelector('[data-value="days"]');
+    this.hours = this.selector.querySelector('[data-value="hours"]');
+    this.mins = this.selector.querySelector('[data-value="mins"]');
+    this.secs = this.selector.querySelector('[data-value="secs"]');
+  }
 
-    const time = this.targetDate - new Date();
+  pageUpdate({ days, hours, mins, secs }) {
+    this.days.textContent = this.addZerro(days);
+    this.hours.textContent = this.addZerro(hours);
+    this.mins.textContent = this.addZerro(mins);
+    this.secs.textContent = this.addZerro(secs);
+  }
 
-    daysRef.textContent = Math.floor(time / (1000 * 60 * 60 * 24));
-    hoursRef.textContent = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    minsRef.textContent = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    secsRef.textContent = Math.floor((time % (1000 * 60)) / 1000);
-  };
-};
+  timeUpdate(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    return { days, hours, mins, secs };
+  }
+
+  setEndTime() {
+    const time = this.targetDate - Date.now();
+    const data = this.timeUpdate(time);
+    this.pageUpdate(data);
+  }
+
+  showTimer() {
+    setInterval(() => {
+      this.setEndTime();
+    }, 1000);
+    this.setEndTime();
+  }
+
+  addZerro(data) {
+    return String(data).padStart(2, '0');
+  }
+}
 
 const timerCountdown = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2021'),
 });
 
-setInterval(() => {
-  timerCountdown.showTimer();
-}, 1000);
+timerCountdown.showTimer();
